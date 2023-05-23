f0c1cd4ae318c8f36c717ca1dc15215539cb8ef6
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-05-22 22:53:16 -0400

    feat: Creates board playing board for the user

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secretSauce"
 
 boggle_game = Boggle()
+
+
+@app.route("/")
+def display_homepage():
+    board = boggle_game.make_board()
+
+    return render_template("index.html", board=board)
