05bc7b6cea6815b59c5fe923ba5ccf2e2204755f
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-05-23 11:47:20 -0400

    feat: Allows user to type a word and submit for it to be checked if valid

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, jsonify
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretSauce"
@@ -10,5 +10,15 @@ boggle_game = Boggle()
 @app.route("/")
 def display_homepage():
     board = boggle_game.make_board()
+    session["board"] = board
 
     return render_template("index.html", board=board)
+
+
+@app.route("/check-word")
+def word_check():
+    word = request.args["word"]
+    board = session["board"]
+    verify_word = boggle_game.check_valid_word(board, word)
+
+    return jsonify({"result": verify_word})
